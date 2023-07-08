da3b2637a3aabf9c0e7ad1fa910fe7868154b2a7
doluong <doluong2007@gmail.com>
2023-07-08 16:10:53 +0700

    local image is ok

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 async def read_main():
     return {"msg": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
--- app/test/test_main.py
+++ app/test/test_main.py
@@ -8,3 +8,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+# def test_read_main():
+#     response = client.get("/items/{id}")
+#     assert response.status_code == 200
+#     assert response.json() == {"items": id}
